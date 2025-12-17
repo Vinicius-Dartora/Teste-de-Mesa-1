1a10af6d78475c236e7e66d596f836b5eb489926
Vinicius Dartora - E. Squad Inovação - Employer <viniciusdartora@employer.com.br>
2025-12-17 15:44:27 -0300

    Teste-de-mesa-1

--- Atividade-1.cs
+++ Atividade-1.cs
@@ -4,23 +4,33 @@ public class Program
 {
     public static void Main()
     {
-        int a = 9;
+        int a = 10;
         int b = 20;
         int c = (a + b) / 2;
-        c = c - 40;
+        int d = c - 40;
         int[] v = new int[6];
+        v[3] = 5;
 
-        v[3] = a + b + c;
+        Console.WriteLine($" {"a",-4} | {"b",-4} | {"c=((a+b)/2)",-10} | {"c=c-40",-4} | {"v(i)",-4} | {"v[i]",-4} | {"v[i]==5",-4}  |");
+        Console.WriteLine("--------------------------------------------------------------");
 
-        if (v[3] == 5)
+        int i = 0;
+        bool t = false;
+        while (t == false && i < 6)
+
         {
-            Console.WriteLine($"O resultado final é: {v[3]}, nesse caso o resultado é VERDADEIRO");
-        }
+            bool condicao = (v[i] == 5);
 
-        else
-       {
-            Console.WriteLine($"O resultado final é: {v[3]} nesse caso o resultado é FALSO");
-        }
 
+            Console.WriteLine($" {a,-5}|{b,-6}|{c,-12} |{d,-7} |{i,-5} |{v[i],-6}| {condicao}");
+
+            if (condicao == true)
+
+            {
+                t = true;
+            }
+
+            i++;
+        }
     }
 }
--- Atividade-2.cs
+++ Atividade-2.cs
@@ -4,24 +4,25 @@ public class Program
 {
     public static void Main()
     {
-        int[] v = new int[6];
-        int b = 20;
         int a = 2;
+        int[] v = new int[6];
 
+        Console.WriteLine("a\t| v[a]\t| a++\t| a<6");
+        Console.WriteLine("----------------------------------------");
         while (a < 6)
-        {
-            int c = (a + b) / 2;
-            c = c - 40;
 
-            v[a] = a + b + c;
+        {
+            v[a] = 10 * a;
+
+            int valor = a;
+            int guardado = v[a];
+            int prox = a + 1;
+            bool condicao = a < 6;
+
+            Console.WriteLine($"{valor}\t|{guardado}\t|{prox}\t|{condicao}");
 
             a += 1;
         }
 
-        Console.WriteLine("Valores finais no vetor:");
-        Console.WriteLine($"Posição v[2]: {v[2]}");
-        Console.WriteLine($"Posição v[3]: {v[3]}");
-        Console.WriteLine($"Posição v[4]: {v[4]}");
-        Console.WriteLine($"Posição v[5]: {v[5]}");
     }
 }
